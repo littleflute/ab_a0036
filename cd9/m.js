25efe29c08ae3aaf00f065710d285625782eae6d
littleflute <13699175164@sina.cn>
2017-07-06 17:49:39 -0500

    Add files via upload

---
+++ cd9/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "ab_a36_cd9_cd9_01.mp3";		
+f[2]	= "ab_a36_cd9_cd9_02.mp3";		
+f[3]	= "ab_a36_cd9_cd9_03.mp3";		
+f[4]	= "ab_a36_cd9_cd9_04.mp3";		
+f[5]	= "ab_a36_cd9_cd9_05.mp3";		
+f[6]	= "ab_a36_cd9_cd9_06.mp3";		
+f[7]	= "ab_a36_cd9_cd9_07.mp3";		
+f[8]	= "ab_a36_cd9_cd9_08.mp3";		
+f[9]	= "ab_a36_cd9_cd9_09.mp3";		
+f[10]	= "ab_a36_cd9_cd9_10.mp3";		
+f[11]	= "ab_a36_cd9_cd9_11.mp3";		
+f[12]	= "ab_a36_cd9_cd9_12.mp3";		
