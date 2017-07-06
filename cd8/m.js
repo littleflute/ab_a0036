076bd987b308de0f9678be6e1801cfcc53ba3c8e
littleflute <13699175164@sina.cn>
2017-07-06 17:49:23 -0500

    Add files via upload

---
+++ cd8/m.js
@@ -0,0 +1,13 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "ab_a36_cd8_cd8_01.mp3";		
+f[2]	= "ab_a36_cd8_cd8_02.mp3";		
+f[3]	= "ab_a36_cd8_cd8_03.mp3";		
+f[4]	= "ab_a36_cd8_cd8_04.mp3";		
+f[5]	= "ab_a36_cd8_cd8_05.mp3";		
+f[6]	= "ab_a36_cd8_cd8_06.mp3";		
+f[7]	= "ab_a36_cd8_cd8_07.mp3";		
+f[8]	= "ab_a36_cd8_cd8_08.mp3";		
+f[9]	= "ab_a36_cd8_cd8_09.mp3";		
+f[10]	= "ab_a36_cd8_cd8_10.mp3";		
+f[11]	= "ab_a36_cd8_cd8_11.mp3";		
