6ce747707f595df53431d7e3eca609b614a45259
littleflute <13699175164@sina.cn>
2017-07-06 17:48:05 -0500

    Add files via upload

---
+++ cd4/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "ab_a36_cd4_cd4_01.mp3";		
+f[2]	= "ab_a36_cd4_cd4_02.mp3";		
+f[3]	= "ab_a36_cd4_cd4_03.mp3";		
+f[4]	= "ab_a36_cd4_cd4_04.mp3";		
+f[5]	= "ab_a36_cd4_cd4_05.mp3";		
+f[6]	= "ab_a36_cd4_cd4_06.mp3";		
+f[7]	= "ab_a36_cd4_cd4_07.mp3";		
+f[8]	= "ab_a36_cd4_cd4_08.mp3";		
+f[9]	= "ab_a36_cd4_cd4_09.mp3";		
+f[10]	= "ab_a36_cd4_cd4_10.mp3";		
